b3388bd912ac7c509a4a9b29657cba8769c46ff9
George Bazerashvili <bazerashviligeorge2000@gmail.com>
2023-09-29 13:01:22 +0400

    origin updated

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import AdminPanel from "./pages/AdminPanel";
 import axios from "axios";
 
 function App() {
-  axios.defaults.baseURL = "https://ecommerce-rest-7nuu.onrender.com";
+  axios.defaults.baseURL = "https://ecommerce-rest-7nuu.onrender.com/";
 
   return (
     <Routes>
